733e1c20b0c4576f16b3aab77f2631330d0f1d4c
Gilberto Azevedo Jr <gilberto-junior@outlook.com>
2022-06-20 13:41:57 -0300

    atualização até conteúdo de Variaveis e Tipos

---
+++ Módulo 3 - Dominando a linguagem de programação javascript/Variáveis e Tipos/AtividadePratica/Resposta/palindromo.js
@@ -0,0 +1,27 @@
+//Atividade 1
+
+//Solução 1
+function verificaPalindromo(string){
+    if (!string) return;
+
+    return string.split("").reverse().join("") === string; // separou as letras da palavra em um array / reverteu a ordem / juntou tudo
+        // por ultimo verificou se era igual
+}
+
+//console.log(verificaPalindromo("roma"));
+
+//Solução 2
+
+function verificaPalindromo2(string){
+    if (!string) return "string inexistente";
+
+        //verifica se é palindromo através dos extremos correspondentes.
+        for (let index = 0; index < string.length; index++) {
+            if (string[index] !== string[string.length - 1 - index]) { // irá verifica se o primeiro é igual ao último e sucessivamente
+                return false;
+            }        
+        }
+        return true;
+}
+
+console.log(verificaPalindromo2("abba"));
